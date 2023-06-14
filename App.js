b375c12d808539e1289a3309e7562abcd8f8020e
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-14 17:43:04 -0300

    Working with Icons

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Teste app</Text>
+      <Ionicons name="home" size={25} color="blue" />
+      <FontAwesome5 name="user-tie" size={24} color="black" />
+      <Feather name="gift" size={24} color="black" />
+
+      <TouchableOpacity style={styles.btnYoutube}>
+        <Feather name="youtube" size={24} color="black" />
+        <Text style={styles.btnText}>Acessar canal</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +23,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btnYoutube:{
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: 'center',
+    padding: 5,
+    backgroundColor: 'red',
+    borderRadius: 5,
+  },
+  btnText:{
+    paddingLeft: 10,
+    color: 'white'
+  }
 });
